706a1962289b18f071f6c35cec5dbfe2d0043ccf
Jaxon Adams <jadamsresume1@gmail.com>
2024-06-12 20:42:44 -0600

    cleaned up menu system

--- src/euler_project_solutions/core.clj
+++ src/euler_project_solutions/core.clj
@@ -8,19 +8,20 @@
 (defn prompt-menu
   "Print the menu of available solutions to run."
   [solutions]
+  (println)
   (println "Please select a solution to run.")
   (loop [to-print solutions]
     (let [[option-id option-desc] (first to-print)
           remaining (rest to-print)]
       (println (str "  [" option-id "] - " option-desc))
       (when (seq remaining)
-        (recur remaining)))))
+        (recur remaining))))
+  (println))
 
 (defn select-problem
   "Prompt the user for the solution to run."
   [options]
   (prompt-menu (map (fn [opt] [(:problem-id opt) (:title opt)]) options))
-  (print " >> ")
   (let [selection (Integer/parseInt (read-line))
         selected (first (filter (fn [opt] (= (:problem-id opt) selection)) options))]
     (apply (:execute selected) (:input selected))))
@@ -32,4 +33,8 @@
                        :title "Multiples of 3 or 5"
                        :input [1000]
                        :execute multiples-of-three-or-five}]]
-    (select-problem menu-options)))
+    (try
+      (let [solution (select-problem menu-options)]
+        (println (str "Solution: " solution)))
+      (catch NullPointerException _ (println "Not a valid selection."))
+      (catch NumberFormatException _ (println "Please make a selection by entering a valid number.")))))
